9e4b9058c707dc71acf367fa02e6b7fb871cf0c3
zhtipu1 <zahidul.haque.bba@ulab.edu.bd>
2026-06-03 20:51:32 +0600

    Update GatekeeperBypassApp.swift

--- GatekeeperBypass/GatekeeperBypassApp.swift
+++ GatekeeperBypass/GatekeeperBypassApp.swift
@@ -1,13 +1,22 @@
 import SwiftUI
+import AppKit
+
+final class AppDelegate: NSObject, NSApplicationDelegate {
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+}
 
 @main
 struct GatekeeperBypassApp: App {
+    @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
         .windowStyle(.hiddenTitleBar)
         .windowResizability(.contentSize)
-        .defaultSize(width: 420, height: 300)
+        .defaultSize(width: 420, height: 340)
     }
 }
